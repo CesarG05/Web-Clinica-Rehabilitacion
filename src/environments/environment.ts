baeb30241c542b3e17d384662ca326245a8f2986
Cesar Chan <chantuncesar@gmail.com>
2026-02-08 14:29:53 -0600

    Delete enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { APP_ID } from "@angular/core";
 
 export const environment = {
   production: true,
-  apiUrl: 'https://ijlmqhny15.execute-api.us-east-1.amazonaws.com/Prod/api'
+  apiUrl: ''
 };
 
